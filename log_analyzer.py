d73aa840520a9dcadf7a6aca76a60405ccd016de
00abhimanyusharma-rgb <00abhimanyusharma@gmail.com>
2026-06-11 07:41:20 +0530

    Create log_analyzer.py

---
+++ log_analyzer.py
@@ -0,0 +1,17 @@
+def summarize():
+
+    print("Incident Summary")
+    print("----------------")
+
+    print("Shipment delays detected")
+    print("HTTP 500 errors detected")
+
+    print("\nProbable Root Cause")
+    print("----------------")
+    print("Shipment service failure")
+
+    print("\nRecommendation")
+    print("----------------")
+    print("Enable retry and monitoring")
+
+summarize()
